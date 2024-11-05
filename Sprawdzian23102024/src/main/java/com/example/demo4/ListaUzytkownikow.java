a80edd8f3a1422087ea8ea796c417924bc2fe219
Efkey <95175771+Efkey2137@users.noreply.github.com>
2024-11-05 13:11:53 +0100

    Sprawdzian 23.10.2024

---
+++ Sprawdzian23102024/src/main/java/com/example/demo4/ListaUzytkownikow.java
@@ -0,0 +1,9 @@
+package com.example.demo4;
+
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+
+public class ListaUzytkownikow {
+    public static ArrayList<Uzytkownik> uzytkownicy = new  ArrayList<>();
+}
